4ab9697d7d1e8d5ff9fa2b5340b3a8090bc69c3a
tsotnyk <sotnykts@gmail.com>
2019-01-04 18:02:06 -0800

    change code

--- LeetCodecom/src/main/java/com/j2core/sts/leetcode/com/n_RepeatedElementInSize2NArray/Solution.java
+++ LeetCodecom/src/main/java/com/j2core/sts/leetcode/com/n_RepeatedElementInSize2NArray/Solution.java
@@ -1,4 +1,24 @@
 package com.j2core.sts.leetcode.com.n_RepeatedElementInSize2NArray;
 
+import java.util.HashMap;
+import java.util.Map;
+
 public class Solution {
+
+    public int repeatedNTimes(int[] A) {
+
+        int N = A.length/2;
+        Map<Integer, Integer> map = new HashMap<>();
+
+        for (int num : A){
+
+            if (map.containsKey(num)){
+                return num;
+            }else {
+                map.put(num, 1);
+            }
+        }
+
+        return 0;
+    }
 }
