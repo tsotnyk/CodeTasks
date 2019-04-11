e6263f50d60d75c9da40c1ff29709b8467182308
tsotnyk <sotnykts@gmail.com>
2019-04-10 20:55:49 -0700

    Add new TASK

--- LeetCodecom/src/main/java/com/j2core/sts/leetcode/com/regularExpressionMatching/Solution.java
+++ LeetCodecom/src/main/java/com/j2core/sts/leetcode/com/regularExpressionMatching/Solution.java
@@ -5,98 +5,82 @@ import org.testng.annotations.Test;
 
 public class Solution {
 
+    public boolean isMatchNew(String s, String p) {
+
+        if (s == null || p == null) {
+            return false;
+        }
+        boolean[][] dp = new boolean[s.length()+1][p.length()+1];
+        dp[0][0] = true;
+        for (int i = 0; i < p.length(); i++) {
+            if (p.charAt(i) == '*' && dp[0][i-1]) {
+                dp[0][i+1] = true;
+            }
+        }
+        for (int i = 0 ; i < s.length(); i++) {
+            for (int j = 0; j < p.length(); j++) {
+                if (p.charAt(j) == '.') {
+                    dp[i+1][j+1] = dp[i][j];
+                }
+                if (p.charAt(j) == s.charAt(i)) {
+                    dp[i+1][j+1] = dp[i][j];
+                }
+                if (p.charAt(j) == '*') {
+                    if (p.charAt(j-1) != s.charAt(i) && p.charAt(j-1) != '.') {
+                        dp[i+1][j+1] = dp[i+1][j-1];
+                    } else {
+                        dp[i+1][j+1] = (dp[i+1][j] || dp[i][j+1] || dp[i+1][j-1]);
+                    }
+                }
+            }
+        }
+        return dp[s.length()][p.length()];
+    }
+
     public boolean isMatch(String s, String p) {
 
-        //TODO
-
-
-        if(!findSymbols(p, p.length())){
-
-            return s.equals(p);
-        }else {
-
-            int indexS = s.length()-1;
-            int indexP = p.length()-1;
-
-            while (indexS > -1 && indexP > -1){
-
-                char tmp = p.charAt(indexP);
-
-                if (tmp == '*'){
-
-                    tmp = p.charAt(--indexP);
-                    if (tmp == '.'){
-                        indexS = 0;
-                    }else {
-                        int tmpIndex = indexS;
-                        while (tmpIndex > -1 && s.charAt(tmpIndex) == tmp) {
-                            tmpIndex--;
-                        }
-                        indexS = ++tmpIndex;
-
-                    }
-                }else if (tmp != '.'){
-                    if (s.charAt(indexS) != p.charAt(indexP)){
-                        return false;
-                    }
-                }
-
-                indexP--;
-                indexS--;
-            }
-
-            if (indexP == -1 && indexS == -1){
-                return true;
-            }else if (indexP == -1){
-                return false;
-            }else {
-
-                String subStr = p.substring(0, indexP+1);
-                if (subStr.indexOf('*') < 0){
-                    return false;
-                }else {
-                    return zeroElements(subStr);
-                }
-            }
-        }
+        return isMatchHelper(s,0, p,0);
 
     }
 
-    private boolean zeroElements(String string){
+    private boolean isMatchHelper(String str,int indexStr,String pattern,int indexPat) {
 
-        int index = string.length()-1;
-        while (index > -1){
+        if( indexPat == pattern.length()) {
 
-            if (string.charAt(index--) != '*' && (!Character.isLetter(string.charAt(index)) || string.charAt(index) != '.')){
-                return false;
+            return indexStr == str.length();
+        }
+
+        char patChar;
+
+        if(indexPat < pattern.length()-1 && pattern.charAt(indexPat+1) == '*') {
+
+            patChar = pattern.charAt(indexPat);
+            if(indexStr < str.length() && (patChar == '.' || str.charAt(indexStr) == patChar)){
+
+                if(isMatchHelper(str,indexStr+1, pattern, indexPat)){
+                    return true;
+                }
             }
-            index--;
-        }
-        return true;
-    }
 
+            return isMatchHelper(str, indexStr, pattern, indexPat+2);
 
-    private boolean findSymbols(String string, int length){
-
-        String str;
-
-        if (string.length() == length){
-            str = string;
-
-        }else {
-
-            str = string.substring(0, length);
         }
 
-        return str.indexOf('*') >= 0 || str.indexOf('.') >= 0;
+        patChar = pattern.charAt(indexPat);
 
+        if(indexStr < str.length() && str.charAt(indexStr) == patChar || patChar == '.') {
+
+            return isMatchHelper(str,indexStr+1, pattern,indexPat+1);
+        }
+
+        return false;
     }
 
 
     @Test
     public void test(){
 
-        Assert.assertTrue(isMatch("aaa", "ab*ac*a"));
+        Assert.assertTrue(isMatch("aa", "a*"));
 
     }
 }
