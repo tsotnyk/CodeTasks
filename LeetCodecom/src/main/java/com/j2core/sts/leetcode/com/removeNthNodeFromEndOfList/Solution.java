ef335f2f236e779cc492b1b987cf0205c0a5f98f
tsotnyk <sotnykts@gmail.com>
2019-04-24 08:18:50 -0700

    add changes

--- LeetCodecom/src/main/java/com/j2core/sts/leetcode/com/removeNthNodeFromEndOfList/Solution.java
+++ LeetCodecom/src/main/java/com/j2core/sts/leetcode/com/removeNthNodeFromEndOfList/Solution.java
@@ -1,4 +1,50 @@
 package com.j2core.sts.leetcode.com.removeNthNodeFromEndOfList;
 
 public class Solution {
+
+    public ListNode removeNthFromEnd(ListNode head, int n) {
+            if (head == null || (head.next == null && n == 1)) {
+                return null;
+            }
+
+            int size = 1;
+            ListNode tmp = head;
+
+            while (tmp.next != null){
+                size++;
+                tmp = tmp.next;
+
+            }
+
+            if (n == size){
+                return head.next;
+            }
+
+            int removeIndex = size-n-1;
+
+            tmp = head;
+            for (int i = 0; i < removeIndex; i++){
+                tmp = tmp.next;
+            }
+
+            if (n == 1){
+                tmp.next = null;
+                return head;
+            }
+
+            tmp = tmp.next;
+
+            while (true){
+
+                tmp.val = tmp.next.val;
+                if (tmp.next.next == null){
+                    tmp.next = null;
+                    break;
+                }
+                tmp = tmp.next;
+            }
+
+            return head;
+
+    }
 }
--- LeetCodecom/src/main/java/com/j2core/sts/leetcode/com/sortColors/Solution.java
+++ LeetCodecom/src/main/java/com/j2core/sts/leetcode/com/sortColors/Solution.java
@@ -1,4 +1,48 @@
 package com.j2core.sts.leetcode.com.sortColors;
 
+import org.testng.annotations.Test;
+
+import java.util.Arrays;
+
 public class Solution {
+
+    public void sortColors(int[] nums) {
+
+        int[] tmp = Arrays.copyOf(nums, nums.length);
+
+        int whiteIndex = 0;
+        int blueIndex = nums.length-1;
+        nums[whiteIndex] = 1;
+        nums[blueIndex] = 1;
+
+        for (int num : tmp){
+
+            if (num == 0){
+                nums[whiteIndex++] = 0;
+                if (whiteIndex < blueIndex){
+                    nums[whiteIndex] = 1;
+                }
+            }else if (num == 2){
+                nums[blueIndex--] = 2;
+                if (whiteIndex < blueIndex){
+                    nums[blueIndex] = 1;
+                }
+            }
+        }
+
+        if (whiteIndex < blueIndex){
+            while (whiteIndex < blueIndex){
+                nums[whiteIndex++] = 1;
+            }
+        }
+    }
+
+    @Test
+    public void test(){
+
+        int[] array = new int[]{1,2,2,2,2,0,0,0,1,1};
+        sortColors(array);
+
+        System.out.println();
+    }
 }
