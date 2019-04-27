a7e959e105f9f50f40043a8efa45d5571cfd39af
tsotnyk <sotnykts@gmail.com>
2019-04-26 19:40:40 -0700

    add a code task

--- LeetCodecom/src/main/java/com/j2core/sts/leetcode/com/pow_x_n/Solution.java
+++ LeetCodecom/src/main/java/com/j2core/sts/leetcode/com/pow_x_n/Solution.java
@@ -1,4 +1,42 @@
 package com.j2core.sts.leetcode.com.pow_x_n;
 
+
 public class Solution {
+
+    public double myPow(double x, int n) {
+        if(x == 1) {
+            return x;
+        }
+        if(x == -1) {
+            return n % 2 == 0 ? 1 : -1;
+        }
+        if(n == 0) {
+            return 1;
+        }
+        if(n < 0) {
+            x = 1/x;
+            n *= -1;
+        }
+        return helper(x, n);
+    }
+
+    private double helper(double x, int n) {
+        if (x == 0) {
+            return 0;
+        }
+        if (n == 1) {
+            return x;
+        }
+        if (n == 0) {
+            return 1;
+        }
+
+        double output = helper(x, n/2);
+
+        if(n % 2 == 0) {
+            return output * output;
+        }else {
+            return output * output * x;
+        }
+    }
 }
