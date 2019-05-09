8af16e3e3255155d3804aefe021cb6ed5de1203d
tsotnyk <sotnykts@gmail.com>
2019-05-08 19:42:39 -0700

    add solution

--- LeetCodecom/src/main/java/com/j2core/sts/leetcode/com/divideTwoIntegers/Solution.java
+++ LeetCodecom/src/main/java/com/j2core/sts/leetcode/com/divideTwoIntegers/Solution.java
@@ -1,80 +1,45 @@
 package com.j2core.sts.leetcode.com.divideTwoIntegers;
 
-import org.junit.Test;
-import org.testng.Assert;
-
 public class Solution {
 
     public int divide(int dividend, int divisor) {
 
-        if (divisor == 1) return dividend;
-
-        if (dividend == divisor) return 1;
-
-        if (dividend == Integer.MIN_VALUE && divisor == -1) return Integer.MAX_VALUE;
-
-        int result = 0;
-        boolean negative;
-
-        if (dividend > 0 && divisor > 0){
-            negative = false;
-        }else if (dividend < 0 && divisor < 0){
-            negative = false;
-            if (dividend == Integer.MIN_VALUE){
-                dividend = (Integer.MAX_VALUE - divisor) + 1;
-                result = 1;
-            }
-            dividend = Math.abs(dividend);
-            divisor  = Math.abs(divisor);
-        }else if (dividend < 0){
-
-            if (dividend == Integer.MIN_VALUE){
-
-                dividend = (Integer.MAX_VALUE - divisor) + 1;
-                result = 1;
-
-            }
+        int result = 0, min = -2147483648, max = 2147483647;
+        if(dividend == min && divisor == -1){
+            return max;
+        }
+        boolean negative = true;
+        if(dividend >= 0 && divisor > 0){
+            dividend = 0 - dividend;
+            divisor = 0 - divisor;
             negative = true;
-            dividend = Math.abs(dividend);
-        }else {
-            negative = true;
-            divisor = Math.abs(divisor);
+        } else if(dividend >= 0 && divisor < 0){
+            dividend = 0 - dividend;
+            negative = false;
+        } else if(dividend < 0 && divisor > 0){
+            divisor = 0 - divisor;
+            negative = false;
         }
 
-//        while (dividend >= divisor){
-//
-//            result++;
-//            dividend -= divisor;
-//        }
-
-        if (dividend < divisor) return 0;
-
-        int sum = divisor;
-        int countSum = 1;
-
-        while (dividend-divisor >= sum){
-
-            dividend -= divisor;
-            result++;
-
-            if (dividend-divisor >= sum){
-                sum += divisor;
-                countSum++;
+        int bdivisor = divisor;
+        while(dividend <= divisor){
+            int i = 0;
+            while(dividend <= divisor){
+                int j = divisor;
+                j <<= 1;
+                if(dividend <= j && j<0){
+                    divisor = j;
+                    i++;
+                }else{
+                    break;
+                }
             }
-
+            result-=Math.pow(2,i);
+            dividend = dividend - divisor;
+            divisor = bdivisor;
         }
 
-
-        return negative ? (result+countSum)*-1 : result+countSum;
-    }
-
-    @Test
-    public void test(){
-
-        int result = divide(-2147483648, -1);
-
-        Assert.assertEquals(result, 3);
+        return negative ? -result : result;
 
     }
-
 }
