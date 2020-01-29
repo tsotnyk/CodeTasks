4d6be0b14aae07bc5679b3741dfb9792b58be54a
Tetiana Sotnyk <sts@Dmytros-MBP-2.attlocal.net>
2020-01-28 17:07:35 -0800

    add task

---
+++ LeetCodecom/src/main/java/com/j2core/sts/leetcode/com/findCityWithSmallestNumberOfNeighborsAtThresholdDistance/Solution.java
@@ -0,0 +1,9 @@
+package com.j2core.sts.leetcode.com.findCityWithSmallestNumberOfNeighborsAtThresholdDistance;
+
+public class Solution {
+
+    public int findTheCity(int n, int[][] edges, int distanceThreshold) {
+
+
+    }
+}
