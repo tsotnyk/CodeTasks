be9c91d57ad459bf9bf69950ff0d9d3c02eef4bd
Tetiana Sotnyk <sts@Dmytros-MBP-2.attlocal.net>
2020-02-03 17:13:57 -0800

    add tasks

---
+++ LeetCodecom/src/main/java/com/j2core/sts/leetcode/com/airplaneSeatAssignmentProbability/Solution.java
@@ -0,0 +1,4 @@
+package com.j2core.sts.leetcode.com.airplaneSeatAssignmentProbability;
+
+public class Solution {
+}
---
+++ LeetCodecom/src/main/java/com/j2core/sts/leetcode/com/maximumScoreWordsFormedByLetters/Solution.java
@@ -0,0 +1,4 @@
+package com.j2core.sts.leetcode.com.maximumScoreWordsFormedByLetters;
+
+public class Solution {
+}
