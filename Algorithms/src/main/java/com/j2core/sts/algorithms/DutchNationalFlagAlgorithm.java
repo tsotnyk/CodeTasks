fbec853b45156a4dbe074f73571bb7bebb6c50ae
Tetiana Sotnyk <sts@Dmytros-MacBook-Pro-2.local>
2020-02-27 17:00:04 -0800

    add solution for algorithm

---
+++ Algorithms/src/main/java/com/j2core/sts/algorithms/DutchNationalFlagAlgorithm.java
@@ -0,0 +1,32 @@
+package java.com.j2core.sts.algorithms;
+
+public class DutchNationalFlagAlgorithm {
+
+    public void sort012(int a[]){
+        int lo = 0;
+        int hi = a.length - 1;
+        int mid = 0, temp = 0;
+        while (mid <= hi) {
+            switch (a[mid]) {
+                case 0: {
+                    temp = a[lo];
+                    a[lo] = a[mid];
+                    a[mid] = temp;
+                    lo++;
+                    mid++;
+                    break;
+                }
+                case 1:
+                    mid++;
+                    break;
+                case 2: {
+                    temp = a[mid];
+                    a[mid] = a[hi];
+                    a[hi] = temp;
+                    hi--;
+                    break;
+                }
+            }
+        }
+    }
+}
