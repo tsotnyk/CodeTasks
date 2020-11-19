11728be5d31e227ee7e036a29ee8e2550ca0f30a
Tetiana Sotnyk <sotnykts@gmail.com>
2020-11-19 14:37:56 -0800

    add new

--- Hackerrankcom/src/main/java/sts/airbnb/Interview.java
+++ Hackerrankcom/src/main/java/sts/airbnb/Interview.java
@@ -1,2 +1,124 @@
-package sts.airbnb;public class Interview {
-}
+package sts.airbnb;
+
+import org.testng.Assert;
+import org.testng.annotations.Test;
+
+import java.util.HashMap;
+import java.util.LinkedList;
+import java.util.List;
+import java.util.Stack;
+
+public class Interview {
+
+        public static int findSmallestNumOfUserForBonus(int[][] matrix){
+
+            HashMap<Integer, List<Integer>> map = new HashMap<>();
+
+            for(int i = 0; i < matrix.length; i++){
+                List<Integer> list = new LinkedList<>();
+                for( int j = 0; j < matrix.length; j++){
+                    if(matrix[i][j] == 1 && i != j){
+                        list.add(j);
+                    }
+                }
+                map.put(i, list);
+            }
+
+            int counter = 0;
+            boolean[] visitUser = new boolean[matrix.length];
+            Stack<Integer> userStack = new Stack<>();
+
+            int user = unVisitedUser(visitUser);
+            while (user >= 0){
+
+                if(!visitUser[user]){
+                    List<Integer> list = map.get(user);
+                    if(list.isEmpty()){
+                        counter++;
+                    }else{
+                        userStack.addAll(list);
+                    }
+                    visitUser[user] = true;
+                }
+
+                if (userStack.isEmpty()) {
+                    user = unVisitedUser(visitUser);
+                }else {
+                    user = userStack.pop();
+                }
+            }
+            return counter;
+        }
+
+        private static int unVisitedUser(boolean[] visitUser){
+
+            for(int i = 0; i < visitUser.length; i++){
+                if(!visitUser[i]){
+                    return i;
+                }
+            }
+
+            return -1;
+        }
+
+        @Test
+    public void test(){
+
+            int[][] matrix = new int[][]{{0, 0, 0, 0, 0, 0, 0, 0, 0, 0},
+            {0, 0, 0, 0, 0, 0, 0, 0, 0, 0},
+            {0, 0, 0, 0, 0, 0, 0, 0, 0, 1},
+            {0, 0, 0, 1, 0, 1, 0, 1, 0, 0},
+            {0, 0, 0, 0, 0, 0, 0, 0, 1, 0},
+            {0, 0, 0, 0, 0, 0, 0, 0, 1, 0},
+            {0, 0, 0, 0, 0, 0, 1, 0, 0, 0},
+            {0, 0, 0, 0, 1, 0, 0, 0, 0, 0},
+            {0, 0, 0, 0, 0, 0, 0, 1, 0, 0},
+            {0, 0, 0, 1, 0, 0, 1, 0, 0, 0}};
+            Assert.assertEquals(findSmallestNumOfUserForBonus(matrix), 3);
+        }
+
+    }
+//1. User i may follow user j, but user j may not follow user i.
+//2. We want to send referral bonus to a subset of users to ask them to broadcast a new feature such that all N users will know this feature in the end.
+//3. We assume that if user i sends a tweet, all the users following i will be notified and all of these followers will broadcast to its' followers and so on.
+//4. We want to find the minimum number of users we want to send referral bonus such that all N users will be notified about this new feature.//
+
+/*
+if  entry (i, j) = 1, user *j *follows user *i*.
+
+matrix.length == matrix[0].length;
+
+input =
+  0 , 1, 2, 3, 4, 5, 6, 7, 8, 9
+[[0, 0, 0, 0, 0, 0, 0, 0, 0, 0], 0
+ [0, 0, 0, 0, 0, 0, 0, 0, 0, 0], 1
+ [0, 0, 0, 0, 0, 0, 0, 0, 0, 1], 2 (2, 9)  9 --> 2
+ [0, 0, 0, 1, 0, 1, 0, 1, 0, 0], 3  (3, )
+ [0, 0, 0, 0, 0, 0, 0, 0, 1, 0], 4
+ [0, 0, 0, 0, 0, 0, 0, 0, 1, 0], 5
+ [0, 0, 0, 0, 0, 0, 1, 0, 0, 0], 6
+ [0, 0, 0, 0, 1, 0, 0, 0, 0, 0], 7
+ [0, 0, 0, 0, 0, 0, 0, 1, 0, 0],8
+ [0, 0, 0, 1, 0, 0, 1, 0, 0, 0]] 9
+
+counter
+
+ 0, ();  counter++;
+ 1 ();   counter++;
+ 2 (9);  continios;
+ 3 (5,7); continios;
+ 4 (8) continios;
+ 5 (8)continios;
+ 6 () counter++;
+ 7 (4); continios;
+ 8(7); continios;
+ 9 (3, 6) continios;
+
+ ======
+                100  (N)  or (10^2) n +  10 n
+ return counter; O(N (amount value in matrix) (n^2) (n - amount user)+ n)  N is number of users in total
+
+ n - amount users
+
+*/
+
--- Hackerrankcom/src/main/java/sts/airbnb/test1/Solution.java
+++ Hackerrankcom/src/main/java/sts/airbnb/test1/Solution.java
@@ -1,2 +1,138 @@
-package sts.airbnb.test1;public class Solution {
+package sts.airbnb.test1;
+
+import org.testng.Assert;
+import org.testng.annotations.Test;
+
+import java.util.List;
+
+public class Solution {
+
+    //1
+    public static List<String> doesCircleExist(List<String> commands) {
+        // Write your code here
+
+
+        return null;
+
+    }
+
+    //2
+    private static int isPresent(Node root, int val){
+
+        if (root == null) return 0;
+        if (root.data == val) return 1;
+
+        if (root.data < val){
+            return isPresent(root.right, val);
+        }else {
+            return isPresent(root.left, val);
+        }
+    }
+
+    class Node {
+        Node left, right;
+        int data;
+        Node(int newData) {
+            left = right = null;
+            data = newData;
+        }
+    }
+
+    //3
+    static String[] triangleOrNot(int[] a, int[] b, int[] c) {
+
+        String[] result = new String[a.length];
+
+        for (int i = 0; i < a.length; i++){
+
+            boolean flag = true;
+            if (a[i]+b[i] <= c[i]){
+                flag = false;
+            }
+            if (flag && b[i] + c[i] <= a[i]){
+                flag = false;
+            }
+            if (flag && a[i] + c[i] <= b[i]){
+                flag = false;
+            }
+            if (flag){
+                result[i] = "Yes";
+            }else {
+                result[i] = "No";
+            }
+        }
+
+        return result;
+    }
+
+    @Test
+    public void test3(){
+
+        Assert.assertEquals(triangleOrNot(new int[]{7,10,7}, new int[]{2,3,4}, new int[]{2,7,4}), new String[]{"No", "No", "Yes"});
+    }
+
+    //4
+    public static int consecutive(long num) {
+        // Write your code here
+
+        int counter = 0;
+        int maxValue = (int)(Math.sqrt(2 * num + 0.25) - 0.5);
+        for (int i = 1; i <= maxValue; ++i) {
+            num -= i;
+            if (num % i == 0)
+                counter++;
+        }
+        return counter-1;
+
+
+//        if (num < 3) return 0;
+//
+//        int counter = 0;
+//        long half = (num/2) +1;
+//        long prevNum = half-1;
+//        long sum = half + prevNum--;
+//
+//        while (sum >= num){
+//
+//            if (sum == num){
+//                counter++;
+//            }
+//            sum -= half--;
+//            while (prevNum > 0 && sum < num){
+//                sum += prevNum--;
+//            }
+//        }
+//
+//        return counter;
+//
+//        int counter = 0;
+//        long startNum = 1;
+//        long endNum = 1;
+//        long sum = 0;
+//
+//        while (startNum <= num/2){
+//            if (sum > num){
+//                sum -= startNum++;
+//            }else{
+//                if (sum == num){
+//                    counter++;
+//                    while (startNum <= endNum/2){
+//                        sum -= startNum++;
+//                    }
+//                }
+//                sum += endNum++;
+//
+//            }
+//        }
+//
+//        return counter;
+    }
+
+    @Test
+    public void test4(){
+
+//        Assert.assertEquals(consecutive(10), 1);
+        Assert.assertEquals(consecutive(15), 3);
+        Assert.assertEquals(consecutive(21), 3);
+    }
 }
