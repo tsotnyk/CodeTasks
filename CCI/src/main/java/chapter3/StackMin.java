14f04cae5cc184f02855897158543c70ae001362
sts <sotnykts@gmail.com>
2023-05-30 14:33:22 -0700

    Add new coding task from CCI

--- CCI/src/main/java/chapter3/StackMin.java
+++ CCI/src/main/java/chapter3/StackMin.java
@@ -1,4 +1,35 @@
 package chapter3;
 
-public class StackMin {
+import java.util.Stack;
+
+public class StackMin extends Stack<Integer>{
+
+    Stack<Integer> minStack = new Stack<>();
+
+    public StackMin() {
+    }
+
+    public void push(int value){
+
+        if (value <= this.minStack.peek()){
+            minStack.push(value);
+        }
+        super.push(value);
+
+    }
+
+    public Integer pop(){
+
+        Integer value = super.pop();
+
+        if (this.minStack.peek().equals(value)){
+            this.minStack.pop();
+        }
+        return value;
+    }
+
+    public Integer min(){
+        return this.minStack.peek();
+    }
+
 }
