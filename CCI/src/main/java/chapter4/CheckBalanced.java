fe70802ba649c69b1a6b31a445d45168505a5aa9
sts <sotnykts@gmail.com>
2023-06-07 16:01:36 -0700

    New coding tasks

--- CCI/src/main/java/chapter4/CheckBalanced.java
+++ CCI/src/main/java/chapter4/CheckBalanced.java
@@ -1,6 +1,49 @@
 package chapter4;
 
+import org.testng.Assert;
+import org.testng.annotations.Test;
+
 public class CheckBalanced {
 
+
+    public boolean isBalanced(MyTree root){
+
+        return depth(root) > -1;
+    }
+
+    public int depth(MyTree root){
+
+        if (root == null) return 0;
+
+        int depthL = depth(root.left);
+        if (depthL < 0) return -1;
+        int depthR = depth(root.right);
+        if (depthR < 0) return -1;
+        if (Math.abs(depthL-depthR) > 1) return -1;
+
+        return Math.max(depthL, depthR)+1;
+    }
+
+    @Test
+    public void test(){
+
+        MyTree root = new MyTree(1);
+        root.left = new MyTree(2);
+        root.right = new MyTree(3);
+        root.left.left = new MyTree(4);
+        root.left.right = new MyTree(5);
+        root.right.left = new MyTree(6);
+        root.right.right = new MyTree(7);
+        root.right.left.left = new MyTree(10);
+        root.left.left.left = new MyTree(8);
+        root.left.right.left = new MyTree(9);
+
+        Assert.assertTrue(isBalanced(root));
+
+        root.right.left.left.left = new MyTree(11);
+
+        Assert.assertFalse(isBalanced(root));
+
+    }
 
 }
--- CCI/src/main/java/chapter4/ValidateBST.java
+++ CCI/src/main/java/chapter4/ValidateBST.java
@@ -1,4 +1,78 @@
 package chapter4;
 
+import org.testng.Assert;
+import org.testng.annotations.Test;
+
 public class ValidateBST {
+
+    public boolean isBST(MyTree root){
+
+        return validation(root, new MinMaxObject()).valid;
+    }
+
+    private MinMaxObject validation(MyTree root, MinMaxObject result){
+
+        if (root == null){
+            return result;
+        }
+
+        result.min = Math.min(result.min, root.value);
+        result.max = Math.max(result.max, root.value);
+
+        if (root.left != null){
+            if (root.left.value > root.value){
+                result.valid = false;
+                return result;
+            }
+        }
+        if (root.right != null){
+            if (root.right.value < root.value){
+                result.valid = false;
+                return result;
+            }
+        }
+        result = validation(root.left, result);
+        if (!result.valid) return result;
+        if (result.max > root.value) {
+            result.valid = false;
+            return result;
+        }
+
+        result = validation(root.right, result);
+        if (!result.valid) return result;
+        if (result.min < root.value) {
+            result.valid = false;
+        }
+        return result;
+    }
+
+    @Test
+    public void test(){
+
+        MyTree root = new MyTree(6);
+        root.right = new MyTree(9);
+        root.right.left = new MyTree(5);
+        root.right.right = new MyTree(10);
+        root.left = new MyTree(3);
+        root.left.left = new MyTree(1);
+        root.left.left.right = new MyTree(2);
+        root.left.right = new MyTree(4);
+
+        Assert.assertFalse(isBST(root));
+    }
+
+
+}
+
+class MinMaxObject{
+
+    int min;
+    int max;
+    boolean valid;
+
+    public MinMaxObject() {
+        this.min = Integer.MAX_VALUE;
+        this.max = Integer.MIN_VALUE;
+        this.valid = true;
+    }
 }
