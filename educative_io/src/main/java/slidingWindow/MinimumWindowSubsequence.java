6a042b975ce521f9949d2134b3062c29bdd427bf
sts <sotnykts@gmail.com>
2023-07-07 11:53:05 -0700

    Add new coding task from CCI

---
+++ educative_io/src/main/java/slidingWindow/MinimumWindowSubsequence.java
@@ -0,0 +1,14 @@
+package slidingWindow;
+
+public class MinimumWindowSubsequence {
+
+    public static String minWindow(String s, String t) {
+
+        String result = "";
+
+        int index1 = 0;
+        int index2 = 0;
+
+        return "";
+    }
+}
